0b6f4581577728c5af157206266960c042b4b27e
GilOnGithub <gmjetomo@outlook.com>
2020-09-28 11:48:25 -0700

    completed fibonacci

--- fibonacciRecursion.js
+++ fibonacciRecursion.js
@@ -5,5 +5,13 @@
 // https://www.mathsisfun.com/numbers/fibonacci-sequence.html#:~:text=The%20Fibonacci%20Sequence%20is%20the,21%2C%2034%2C%20...
 
 const fibonacciRecursive = function(n) {
-
+    if(n === 2) {
+        return [0, 1];
+    } else {
+        let list = fibonacciRecursive(n-1);
+        list.push(list[list.length-1] + list[list.length-2]);
+        return list;
+    }
 }
+
+console.log(fibonacciRecursive(6)); //0, 1, 1, 2, 3
